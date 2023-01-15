1500527f19caf56f314ad9e233f0c6799711932d
bodowd <odowd.bing@gmail.com>
2023-01-15 07:37:55 +0100

    Refactor so that main is simpler
    Didn't need a main model to hold other models because we start from the
    list page. The list page will link us to the new_habit page. the list
    page is the "home page"

--- main.go
+++ main.go
@@ -8,40 +8,8 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-var models []tea.Model
-
-type Model struct {
-	GoToNewEntryPage bool
-	ListPage         pages.ListModel
-	NewEntryPage     pages.TextInputModel
-}
-
-func NewModel() Model {
-	m := Model{
-		GoToNewEntryPage: false,
-		ListPage:         pages.NewList(),
-	}
-
-	return m
-}
-
-func (m Model) Init() tea.Cmd {
-	return nil
-}
-
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return m.ListPage.Update(msg)
-}
-
-func (m Model) View() string {
-	return m.ListPage.View()
-}
-
 func main() {
-	// p := tea.NewProgram(cmd.InitialModel())
-	// models = []tea.Model{pages.NewTextInputModel(), pages.NewList()}
-	// m := models[0]
-	p := tea.NewProgram(NewModel())
+	p := tea.NewProgram(pages.NewList())
 
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
